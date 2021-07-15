35f4ac1108fe134eb37f6b3e916ed1758794b65c
Kristina Palenykh <kpalenykh@bk.ru>
2021-07-15 21:53:06 +0300

    add games1

--- scr/games1.js
+++ scr/games1.js
@@ -6,14 +6,26 @@ const games1 = () => {
   for (let i = 1; i <= 3; i += 1) {
     const rand = Math.round(Math.random() * Math.random() * 100);
     const otvet = readlineSync.question(`${rand} `);
-    if (otvet === 'no' && rand % 2 === 0) {
+    if ((otvet === 'no' || otvet === 'n' || otvet === ' ') && rand % 2 === 0) {
       console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
       console.log(`Let's try again, ${name}!`);
       break;
     }
 
-    if (otvet === 'yes' && rand % 2 > 0) {
+    if ((otvet === 'yes' || otvet === 'y' || otvet === 'ye' || otvet === ' ') && rand % 2 > 0) {
       console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+
+    if ((otvet === 'y' || otvet === 'ye' || otvet === ' ') && rand % 2 === 0) {
+      console.log(`'${otvet}' is wrong answer ;(. Correct answer was 'yes'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+
+    if ((otvet === 'n' || otvet === ' ') && rand % 2 > 0) {
+      console.log(`'${otvet}' is wrong answer ;(. Correct answer was 'no'.`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
