83cba5a1e5e0393f56d9df8226803c0841c543e8
Kristina Palenykh <kpalenykh@bk.ru>
2021-07-16 15:19:31 +0300

    calculater

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 import { name } from '../scr/cli.js';
-import games1 from '../scr/games1.js';
+import chetnost from '../scr/chetnost.js';
 
-games1(name);
+chetnost(name);
--- scr/chetnost.js
+++ scr/chetnost.js
@@ -1,31 +1,19 @@
 import readlineSync from 'readline-sync';
 import { name } from './cli.js';
 
-const games1 = () => {
+const chetnost = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
     const rand = Math.round(Math.random() * Math.random() * 100);
     const otvet = readlineSync.question(`${rand} `);
-    if ((otvet === 'no' || otvet === 'n' || otvet === ' ') && rand % 2 === 0) {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    if (otvet !== 'yes' && rand % 2 === 0) {
+      console.log(`${otvet} is wrong answer ;(. Correct answer was 'yes'.`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
 
-    if ((otvet === 'yes' || otvet === 'y' || otvet === 'ye' || otvet === ' ') && rand % 2 > 0) {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-
-    if ((otvet === 'y' || otvet === 'ye' || otvet === ' ') && rand % 2 === 0) {
-      console.log(`'${otvet}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-
-    if ((otvet === 'n' || otvet === ' ') && rand % 2 > 0) {
-      console.log(`'${otvet}' is wrong answer ;(. Correct answer was 'no'.`);
+    if (otvet !== 'no' && rand % 2 > 0) {
+      console.log(`${otvet} is wrong answer ;(. Correct answer was 'no'.`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
@@ -38,4 +26,4 @@ const games1 = () => {
   }
 };
 
-export default games1;
+export default chetnost;
