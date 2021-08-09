11a1c8985cf23e07494848777ac36c2635a80776
Kristina Palenykh <kpalenykh@bk.ru>
2021-08-09 14:00:35 +0300

    fix

--- src/brain-start.js
+++ src/brain-start.js
@@ -1,10 +1,8 @@
   
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+export default () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello! ${userName} \n`);
 };
-
-export default greeting;
